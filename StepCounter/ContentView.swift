4499df2f83859dc75e367acc8cf6f51a504d9c04
yumin-jung <jungym887@naver.com>
2025-07-25 13:43:57 +0900

    Initial Commit

---
+++ StepCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepCounter
+//
+//  Created by yumin on 7/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by yumin on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
